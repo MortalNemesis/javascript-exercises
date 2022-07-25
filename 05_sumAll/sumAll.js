cab8a92c3dc69f8d23b3df511f8be9b2dc673b29
MortalNemesis <lkloft@outlook.com>
2022-07-25 16:44:01 -0700

    Pass 05 tests

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,27 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    if (typeof a != "number" || typeof b != "number") {
+        return "ERROR";
+    }
 
+    if (a < 0 || b < 0) {
+        return "ERROR";
+    }
+
+    let result = 0;
+
+    if (a > b) {
+        while(a >= b) {
+            result += a;
+            a--;
+        }
+    } else {
+        while(a <= b) {
+            result += a;
+            a++;
+        }
+    }
+
+    return result;
 };
 
 // Do not edit below this line
