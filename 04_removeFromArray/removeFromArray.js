44719ffbc588c34ab962f245b431dde946df580c
MortalNemesis <lkloft@outlook.com>
2022-07-25 16:38:35 -0700

    Pass 04 tests

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,9 @@
 const removeFromArray = function() {
-
+    let result = arguments[0];
+    for(let i = 1; i < arguments.length; i++) {
+        result = result.filter((item) => item !== arguments[i]);
+    }
+    return result;
 };
 
 // Do not edit below this line
