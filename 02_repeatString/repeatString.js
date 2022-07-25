ddda82275855c323352d28d2073306d477239669
MortalNemesis <lkloft@outlook.com>
2022-07-25 16:18:31 -0700

    Pass 02 tests

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(str, n) {
+    if (n < 0) {
+        return "ERROR";
+    }
+    
+    result = "";
+    while(n > 0) {
+        result += str;
+        n--;
+    }
+    return result;
 };
 
 // Do not edit below this line
